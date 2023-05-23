7536cd1e178311d12aafa3eb058c1abb8f486fc9
OlenaStoliarova <stoliarova.elena@gmail.com>
2023-05-23 08:34:46 +0200

    [#16 - NoSQL Data Storages] task 2 - Create index for searching by field; task 3 - Embedded objects

--- m16-nosql/src/main/java/pl/mentoring/m16nosql/repository/UserRepository.java
+++ m16-nosql/src/main/java/pl/mentoring/m16nosql/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package pl.mentoring.m16nosql.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.couchbase.repository.CouchbaseRepository;
 import org.springframework.stereotype.Repository;
 import pl.mentoring.m16nosql.entity.User;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends CouchbaseRepository<User, String> {
+
+    List<User> findByEmail(String email);
 }
