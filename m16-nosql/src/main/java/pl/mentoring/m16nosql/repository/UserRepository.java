5454eb873243407efbc1f541375d144b14bcbe38
OlenaStoliarova <stoliarova.elena@gmail.com>
2023-05-25 08:29:08 +0200

    [#16 - NoSQL Data Storages] task 4 - Search by embedded object fields

--- m16-nosql/src/main/java/pl/mentoring/m16nosql/repository/UserRepository.java
+++ m16-nosql/src/main/java/pl/mentoring/m16nosql/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package pl.mentoring.m16nosql.repository;
 
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.data.couchbase.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.mentoring.m16nosql.entity.User;
 
@@ -10,4 +11,7 @@ import java.util.List;
 public interface UserRepository extends CouchbaseRepository<User, String> {
 
     List<User> findByEmail(String email);
+
+    @Query("#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND ANY s IN sports SATISFIES s.sportName=$1 END")
+    List<User> findBySportName(String sportName);
 }
